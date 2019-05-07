c7b79b53e9ce221a10da5df7e153dbc0d49274cc
savytskyivasyl <vasyls@wix.com>
2019-05-07 17:18:40 +0300

    add printable exercise

---
+++ src/main/scala/com/faustern/exercises/printable/Cat.scala
@@ -0,0 +1,3 @@
+package com.faustern.exercises.printable
+
+final case class Cat(name: String, age: Int, color: String)
--- src/main/scala/com/faustern/exercises/printable/Printable.scala
+++ src/main/scala/com/faustern/exercises/printable/Printable.scala
@@ -1,5 +1,23 @@
 package com.faustern.exercises.printable
 
-class Printable {
+trait Printable[A] {
+  def format(value: A): String
+}
 
+object PrintableInstances {
+  implicit val IntPrintable: Printable[Int] = (value: Int) => value.toString
+  implicit val StringPrintable: Printable[String] = (value: String) => value.toString
+  implicit val CatPrintable: Printable[Cat] = (value: Cat) => s"${value.name} is a ${value.age} year-old ${value.color} cat"
+}
+
+object Printable {
+  def format[A](value: A)(implicit p: Printable[A]): String = p.format(value)
+  def print[A](value: A)(implicit p: Printable[A]): Unit = println(p.format(value))
+}
+
+object PrintableSyntax {
+  implicit class PrintableOps[A](value: A) {
+    def format(implicit p: Printable[A]): String = p.format(value)
+    def print(implicit p: Printable[A]): Unit = println(p.format(value))
+  }
 }
